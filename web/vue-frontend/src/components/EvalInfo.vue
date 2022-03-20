ce0781591e09a3d912644fddfeba79c77584b781
Nico Windler <nico.windler@gmail.com>
2022-03-20 15:33:21 +0100

    Change eval bar

--- web/vue-frontend/src/components/EvalInfo.vue
+++ web/vue-frontend/src/components/EvalInfo.vue
@@ -6,7 +6,7 @@
 
     <v-progress-linear
       height="50"
-      :value="pawn"
+      :value="getPawnValue()"
       background-color="grey darken-4"
       color="grey lighten"
     >
@@ -20,6 +20,24 @@
 <script>
 export default {
   name: "EvalInfo",
+
+  methods: {
+    getPawnValue() {
+      let base = 50;
+      if (this.pawn == base) { 
+        return base
+        }
+      let advantage = Math.abs(base - this.pawn ) 
+      let translatedAdvantage = base - (base / advantage)
+      if (advantage < 1) {
+        translatedAdvantage = advantage
+      }
+      if (this.pawn < base) {
+        return base - translatedAdvantage
+      }
+      return base + translatedAdvantage
+    }
+  },
 
   props: ["pawn"],
   data() {
